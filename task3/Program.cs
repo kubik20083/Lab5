2d22b032b9c90ca4369e99608e263a3fd4e7ae76
kubik <kubik20083@mail.ru>
2023-01-22 23:00:09 +0300

    решение задача 3

---
+++ task3/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите размер массива  ");
+int size = Convert.ToInt32(Console.ReadLine());
+double[] numbers = new double[size];
+ArrayNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+double min = Double.MaxValue;
+double max = Double.MinValue;
+
+for (int a = 0; a < numbers.Length; a++)
+{
+    if (numbers[a] > max)
+    {
+        max = numbers[a];
+    }
+    if (numbers[a] < min)
+    {
+        min = numbers[a];
+    }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine($"{max} - {min} = {max - min}");
+
+void ArrayNumbers(double[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Random rnd = new Random();
+        numbers[i] = rnd.NextDouble() * 100;
+     
+    }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
