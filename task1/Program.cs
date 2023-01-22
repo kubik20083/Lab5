328bdd9e75905ccec8162faba9ee19fe641dd710
kubik <kubik20083@mail.ru>
2023-01-22 21:55:30 +0300

    решение задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Console.WriteLine("Задайте размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+ArrayNumbers(numbers);
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+    if (numbers[z] % 2 == 0)
+        count++;
+
+System.Console.WriteLine($"В этом массиве {count} четных чисел");
+
+void ArrayNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    System.Console.WriteLine("[ ");
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
